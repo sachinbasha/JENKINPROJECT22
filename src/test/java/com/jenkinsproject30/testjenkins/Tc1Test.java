0488f7188e8e47bcd3479bd353d0e2fe3652e955
sachinbasha <uchesachin96@gmail.com>
2023-11-30 13:04:06 +0530

    commit 30

---
+++ src/test/java/com/jenkinsproject30/testjenkins/Tc1Test.java
@@ -0,0 +1,18 @@
+package com.jenkinsproject30.testjenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc1Test
+{
+	@Test
+	public void test1()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+	   System.out.println(driver.getCurrentUrl());
+	}
+
+}
---
+++ src/test/java/com/jenkinsproject30/testjenkins/Tc2Test.java
@@ -0,0 +1,14 @@
+package com.jenkinsproject30.testjenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2Test
+{
+	@Test
+	public void test()
+	{
+		Reporter.log("testcase2",true);
+	}
+
+}
